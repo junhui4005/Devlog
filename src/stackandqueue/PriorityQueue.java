2356517ce786272701fd73308a668de5ec26c9c4
junhui4005 <100547902+junhui4005@users.noreply.github.com>
2025-04-14 11:48:44 +0900

    우선순위 Queue code

---
+++ src/stackandqueue/PriorityQueue.java
@@ -0,0 +1,96 @@
+package src.stackandqueue;
+
+import java.util.Arrays;
+
+// 최소 힙 동작하는 우선순위 큐
+public class PriorityQueue {
+    private int[] heap;
+    private int size;
+    private int capacity;
+
+    public PriorityQueue(int capacity) {
+        this.capacity = capacity;
+        heap = new int[capacity];
+        size = 0;
+    }
+
+    public boolean isFull() {
+        return size == capacity;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private void heapifyUp(int index) {
+        while (index > 0) {
+            int parent = (index - 1) / 2; // 부모 노드 찾기
+            if (heap[parent] <= heap[index]) {
+                break;
+            }  // 부모가 더 작으면 break
+
+            int temp = heap[parent];
+            heap[parent] = heap[index]; // 부모랑 change
+            heap[index] = temp;
+            index = parent;
+        }
+    }
+
+    private void heapifyDown(int index) {
+        while (index < size){
+            int left = index * 2 + 1;
+            int right = index * 2 + 2;
+            int smallest = index;
+            if (left < size && heap[left] < heap[smallest]) { smallest = left; }
+            if (right < size && heap[right] < heap[smallest]) { smallest = right; }
+            if (smallest == index) { break;}
+
+            int temp = heap[index];
+            heap[index] = heap[smallest];
+            heap[smallest] = temp;
+            index = smallest;
+        }
+    }
+
+    public void offer(int value){
+        if(isFull()){
+            return;
+        }
+        heap[size] = value;
+        heapifyUp(size);
+        size++;
+    }
+
+    public int poll(){
+        if(isEmpty()){
+            return -1;
+        }
+        int min  = heap[0];
+        heap[0] = heap[size-1];
+        size--;
+        heapifyDown(0);
+        return min;
+    }
+    public int peek(){
+        if(isEmpty()){
+            return -1;
+        }
+        return heap[0];
+    }
+
+    public void print(){
+        System.out.println("Priority Queue: " + Arrays.toString(Arrays.copyOf(heap, size)));
+    }
+
+    public static void main(String[] args) {
+        PriorityQueue priorityQueue = new PriorityQueue(5);
+        priorityQueue.offer(3);
+        priorityQueue.offer(2);
+        priorityQueue.print();
+        priorityQueue.offer(1);
+        priorityQueue.print();
+        priorityQueue.poll();
+        priorityQueue.print();
+        System.out.println("peek: " + priorityQueue.peek());
+    }
+}
