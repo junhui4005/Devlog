1c71d2d622c739fcd7b1d3c0116c6e6c6daa8795
junhui4005 <100547902+junhui4005@users.noreply.github.com>
2025-05-04 17:54:33 +0900

    Update Lv1_신고결과받기.java

--- src/codingtest/programmers/Lv1_신고결과받기.java
+++ src/codingtest/programmers/Lv1_신고결과받기.java
@@ -1,5 +1,6 @@
 package src.codingtest.programmers;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
@@ -8,6 +9,7 @@ public class Lv1_신고결과받기 {
     public static void main(String[] args) {
         String[] id_list = {"muzi", "frodo", "apeach", "neo"};
         String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
+        int [] result = new int[id_list.length];
         int k = 2;
 
         Map<String, Map<String,Integer>> warning = new HashMap<>();
@@ -25,6 +27,18 @@ public class Lv1_신고결과받기 {
                 }
             }
         }
-        System.out.println("warning: " + warning);
+        for (int i = 0; i < id_list.length; i++) {
+            String id = id_list[i];
+            int count = warning.get(id).size();
+            if(count >= k){
+               System.out.println(warning.get(id).keySet());
+               for(int j = 0; j < id_list.length; j++){
+                   if(warning.get(id).containsKey(id_list[j])){
+                       result[j] = result[j]+1;
+                   }
+               }
+            }
+        }
+        System.out.println(Arrays.toString(result));
     }
 }
